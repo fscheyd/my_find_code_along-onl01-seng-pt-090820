03e56a0fe2731fc520155532851209a267da830f
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-15 16:45:16 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,7 @@ def my_find(collection)
   binding.pry
   i = 0
   while i < collection.length
-    yield(collection[i])
-    i = i + 1
+    return collection[i] if yield(collection[i])
+      i = i + 1
   end
 end
